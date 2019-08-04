f7305a216ab36258e8c9e84fb5779dd6bb8c5587
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-04 17:39:48 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,30 @@
 class SchoolClassesController < ApplicationController
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.create(post_params(:title, :room_number))
+    redirect_to post_path(@post)
+  end
+
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(post_params(:title))
+    redirect_to school_class_path(@school_class)
+  end
+
+  private
+
+  def post_params(*args)
+    params.require(:school_class).permit(*args)
+  end
 end
