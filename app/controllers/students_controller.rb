f4d9cc3558427c1313aaf6c663fa73f65e99a377
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-04 17:42:31 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,30 @@
 class StudentsController < ApplicationController
+
+  def index
+    @student = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @student = Student.create(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params(:first_name))
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def student_params(*args)
+    params.require(:student).permit(*args)
+  end
 end
