4c9c9dab80e568751be1929c48800a7e9675eee9
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-04 17:43:00 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,19 +12,19 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.create(post_params(:title, :room_number))
+    @school_class = SchoolClass.create(school_class_params(:title, :room_number))
     redirect_to school_class_path(@school_class)
   end
 
   def update
     @school_class = SchoolClass.find(params[:id])
-    @school_class.update(post_params(:title))
+    @school_class.update(school_class_params(:title))
     redirect_to school_class_path(@school_class)
   end
 
   private
 
-  def post_params(*args)
+  def school_class_params(*args)
     params.require(:school_class).permit(*args)
   end
 end
